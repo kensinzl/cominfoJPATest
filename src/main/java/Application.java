681e94c486121c457f89fcebf1291d09d3c3baf5
Unknown <zlchldjyy@gmail.com>
2019-10-28 19:52:51 +1300

    2019-10-28 upload

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -10,12 +10,51 @@ import entity.Employee;
 import entity.Movie;
 import service.EmployeeService;
 import service.MovieService;
+import terminalA.Person;
 
+import java.io.*;
 import java.util.Arrays;
 
 
 public class Application {
     public static void main(String[] args) {
+
+        /**
+         * Serializable and De-serializable
+         */
+        File file = new File ("person.out");
+//        terminalA.Person tom = new terminalA.Person("Tom", 22, "Male");
+//        terminalA.Person.nationality = "British";
+//
+//        try {
+//            // serializable
+//            ObjectOutputStream oout = new ObjectOutputStream(new FileOutputStream(file));
+//            oout.writeObject(tom);
+//            oout.close();
+//        } catch(FileNotFoundException ex) {
+//            System.out.println ("FileNotFoundException: " + ex);
+//        } catch (IOException ex) {
+//            System.out.println ("IOException: " + ex);
+//        }
+
+
+        try {
+            // de-serializable
+            ObjectInputStream oin = new ObjectInputStream(new FileInputStream(file));
+            terminalB.Person personTom = (terminalB.Person)oin.readObject();
+            oin.close();
+            System.out.println(personTom);
+        } catch (FileNotFoundException ex) {
+            System.out.println ("FileNotFoundException: " + ex);
+        } catch (IOException ex) {
+            System.out.println ("IOException: " + ex);
+        } catch (ClassNotFoundException ex) {
+            System.out.println ("ClassNotFoundException: " + ex);
+        }
+
+
+
+
 
         /**
          * Just a small test case for the callBack
@@ -61,86 +100,86 @@ public class Application {
         MovieService movieService = new MovieService();
         EmployeeService employeeService = new EmployeeService();
 
-//        /**
-//         * Persist Action(new instance)
-//         *
-//         * 1. before persist, new movie instance is not contained by persistence context
-//         * 2. after persist, new movie instance is contained by persistence context
-//         */
-//        MovieDto movieDto = createMovieDto ("Three Kings", 2019, "JP");
-//        movieService.saveMovie (movieDto);
-//        System.out.println ("------ persist one movie instance into persistence context ------ ");
-//        System.out.println ("------ insert one movie into database ------ ");
-//
-//
-//        // detach the instance from persistence context
-//        movieService.detachMovieById (1L);
-//        System.out.println ("------ detach this instance from persistence context ------ ");
-//
-//        System.out.println ("------ switch into SQL query to fetch because of detach ------ ");
-//        Movie movieEntityFromDB = movieService.findMovieById (1L);
-//        System.out.println ("------ fetch SQL End ------ ");
-//        System.out.println("------ After find, movieEntityFromDB, em "
-//                + (movieService.isContain (movieEntityFromDB) == true ? "contains " : "not contain ")
-//                + movieEntityFromDB);
-//
-//
-//        Movie movieEntityFromCache = movieService.findMovieById (1L);
-//        System.out.println ("------ find again, movieEntityFromCache is the cache of movieEntityFromDB among the persistence context, "
-//                        + movieEntityFromCache + "No SQL Start ------ ");
-//
-//        movieService.detachMovieById (1L);
-//        System.out.println ("------ detach again ------ ");
-//
-//        System.out.println("------ After find and then detach, movieEntityFromCache, em "
-//                + (movieService.isContain (movieEntityFromCache) == true ? "contains " : "not contain ")
-//                + movieEntityFromCache);
-//        System.out.println("------ After find and then detach, movieEntityFromDB, em "
-//                + (movieService.isContain (movieEntityFromDB) == true ? "contains " : "not contain ")
-//                + movieEntityFromDB);
-//
-//
-//        /**
-//         * Merge Action(return value)
-//         *
-//         * 1. merge(new non attach instance), insert it for database and return new attach instance
-//         * 2. merge(detach instance), update the changes and return new attach instance
-//         * 3. merge(attach instance), upate the changes and return the cache instance
-//         * 4. merge(new instance but attach and exist in persistence context), update and return the cache instance
-//         *
-//         */
-//        MovieDto dto = createMovieDto ("new non attach instance", 2019, "EN");
-//        Movie newAttachInstance = movieService.mergeNewNonAttachIns (dto);
-//
-//        Movie detachToAttachByMerge = movieService.mergeDetachIns (1L);
-//
-//        Movie attachAgainByMerge = movieService.mergeAttachIns (1L);
-//
-//        dto.setId (1L);
-//        dto.setLanguage (dto.getLanguage () + "_new ins but exist");
-//        Movie newInsAttachExistInPC = movieService.mergeNewInsButExistedInPC (dto);
-//
-//
-//        System.out.println("************ ");
-//
-//        // use find or merge to update.
-//        /**
-//         * Update Action
-//         * 1. find
-//         * 2. merge
-//         * 3. find, then change the attribute and merge(better)
-//         */
-//        MovieDto updatingMovieDtoByFind = createMovieDto ("updating movie dto by find", 2019, "CN");
-//        movieService.mergeNewNonAttachIns (updatingMovieDtoByFind);
-//        updatingMovieDtoByFind.setId (3L);
-//        movieService.updateMovieByFind (updatingMovieDtoByFind);
-//
-//        MovieDto updatingMovieDtoMerge = createMovieDto ("updating movie dto by merge", 2019, "CS");
-//        movieService.mergeNewNonAttachIns (updatingMovieDtoMerge);
-//        updatingMovieDtoMerge.setId (4L);
-//        movieService.updateMovieByMerge (updatingMovieDtoMerge);
-//
-//        movieService.updateByFindMerge(4L);
+        /**
+         * Persist Action(new instance)
+         *
+         * 1. before persist, new movie instance is not contained by persistence context
+         * 2. after persist, new movie instance is contained by persistence context
+         */
+        MovieDto movieDto = createMovieDto ("Three Kings", 2019, "JP");
+        movieService.saveMovie (movieDto);
+        System.out.println ("------ persist one movie instance into persistence context ------ ");
+        System.out.println ("------ insert one movie into database ------ ");
+
+
+        // detach the instance from persistence context
+        movieService.detachMovieById (1L);
+        System.out.println ("------ detach this instance from persistence context ------ ");
+
+        System.out.println ("------ switch into SQL query to fetch because of detach ------ ");
+        Movie movieEntityFromDB = movieService.findMovieById (1L);
+        System.out.println ("------ fetch SQL End ------ ");
+        System.out.println("------ After find, movieEntityFromDB, em "
+                + (movieService.isContain (movieEntityFromDB) == true ? "contains " : "not contain ")
+                + movieEntityFromDB);
+
+
+        Movie movieEntityFromCache = movieService.findMovieById (1L);
+        System.out.println ("------ find again, movieEntityFromCache is the cache of movieEntityFromDB among the persistence context, "
+                        + movieEntityFromCache + "No SQL Start ------ ");
+
+        movieService.detachMovieById (1L);
+        System.out.println ("------ detach again ------ ");
+
+        System.out.println("------ After find and then detach, movieEntityFromCache, em "
+                + (movieService.isContain (movieEntityFromCache) == true ? "contains " : "not contain ")
+                + movieEntityFromCache);
+        System.out.println("------ After find and then detach, movieEntityFromDB, em "
+                + (movieService.isContain (movieEntityFromDB) == true ? "contains " : "not contain ")
+                + movieEntityFromDB);
+
+
+        /**
+         * Merge Action(return value)
+         *
+         * 1. merge(new non attach instance), insert it for database and return new attach instance
+         * 2. merge(detach instance), update the changes and return new attach instance
+         * 3. merge(attach instance), upate the changes and return the cache instance
+         * 4. merge(new instance but attach and exist in persistence context), update and return the cache instance
+         *
+         */
+        MovieDto dto = createMovieDto ("new non attach instance", 2019, "EN");
+        Movie newAttachInstance = movieService.mergeNewNonAttachIns (dto);
+
+        Movie detachToAttachByMerge = movieService.mergeDetachIns (1L);
+
+        Movie attachAgainByMerge = movieService.mergeAttachIns (1L);
+
+        dto.setId (1L);
+        dto.setLanguage (dto.getLanguage () + "_new ins but exist");
+        Movie newInsAttachExistInPC = movieService.mergeNewInsButExistedInPC (dto);
+
+
+        System.out.println("************ ");
+
+        // use find or merge to update.
+        /**
+         * Update Action
+         * 1. find
+         * 2. merge
+         * 3. find, then change the attribute and merge(better)
+         */
+        MovieDto updatingMovieDtoByFind = createMovieDto ("updating movie dto by find", 2019, "CN");
+        movieService.mergeNewNonAttachIns (updatingMovieDtoByFind);
+        updatingMovieDtoByFind.setId (3L);
+        movieService.updateMovieByFind (updatingMovieDtoByFind);
+
+        MovieDto updatingMovieDtoMerge = createMovieDto ("updating movie dto by merge", 2019, "CS");
+        movieService.mergeNewNonAttachIns (updatingMovieDtoMerge);
+        updatingMovieDtoMerge.setId (4L);
+        movieService.updateMovieByMerge (updatingMovieDtoMerge);
+
+        movieService.updateByFindMerge(4L);
 
 
         /**
@@ -400,8 +439,8 @@ public class Application {
         return employeeDto;
     }
 
-    /*
-    Test for git
-    second time
-     */
+
+
+
+
 }
---
+++ src/main/java/terminalA/Person.java
@@ -0,0 +1,50 @@
+package terminalA;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 2L;
+
+    private String name;
+    private int age;
+    private String gender;
+    public static String nationality;
+
+    public Person(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonA: [ " + this.age +", "+ this.name +", "+this.gender+ ", " +nationality+" ]";
+    }
+
+
+}
